23758368495db39f608fcd02eb34a46fb13bb6f4
Huntrese <bananananaaa6@gmail.com>
2024-04-02 13:10:04 +0300

    add lab4

---
+++ main_lab4.py
@@ -0,0 +1,95 @@
+import src.lab1.grammarHelper as grammarHelper
+
+import src.lab1.language as language
+
+variant="""
+(a|b)(c|d)E+G?
+P(Q|R|S)T(UV|W|X)*Z+
+1(0|1)*2(3|4)^(5)36
+"""
+grammars=[]
+REGEX = [x for x in variant.split("\n") if x]
+terminals="Sabcdefghijklmnoprtqwxyz"
+nonterminals=terminals.upper()
+print(REGEX)
+for expr in REGEX:
+    expr=expr.lower()
+    final="\n"
+
+    starting=0
+
+    expr=expr.replace("("," ").replace(")"," ").replace("*"," * ").replace("^"," ^ ").replace("+"," + ").replace("?"," ? ")
+    expr=[x for x in expr.split(" ") if x]
+    VT=[]
+    for i,node in enumerate(expr):
+        node=str(node)
+        if "|" in node:
+            choices=node.split("|")
+            for choice in choices:
+                final+=f'{nonterminals[starting]} → {choice}{nonterminals[starting+1]}\n'
+                if choice not in VT:
+                    VT.append(choice)
+        elif node == "+":
+            final+=f'{nonterminals[starting]} → {nonterminals[starting-1]}\n'
+            starting-=1
+
+        elif node == "*":
+            final+=f'{nonterminals[starting-1]} → {nonterminals[starting]}\n'
+            final+=f'{nonterminals[starting]} → {nonterminals[starting-1]}\n'
+            starting-=1
+
+        elif node == "?":
+            final+=f'{nonterminals[starting-1]} → {nonterminals[starting]}\n'
+            starting-=1
+        elif node == "^":
+            times = int(expr[i+1])
+            
+            for j in range(times-1):
+
+                previous=[x for x in final.split("\n") if x]
+                previous = [x for x in previous if (x[0]==nonterminals[starting-1+j])]
+                s=""
+                
+                for x in previous:
+
+                    s+=nonterminals[starting+j]
+                    s+=x[1:-1]
+                    s+=nonterminals[starting+j+1]
+                    s+="\n"
+
+                final+=s
+            starting+= times-2
+
+
+        elif node.isalnum():
+            if expr[i-1] == "^":
+                continue
+            final+=f'{nonterminals[starting]} → {node}{nonterminals[starting+1]}\n'
+            VT.append(node)
+        starting+=1
+    
+    final+=f'{nonterminals[starting]} →  \n'
+
+    # print("Varianta 20:")
+    # print("VN= {",f"{', '.join(list(nonterminals[:starting+1]))}","},")
+    # print("VT= {",f"{', '.join(VT)}","},")
+    # print("P={")
+    # print(final)
+    # print("}")
+    lb="{"
+    rb="}"
+    grammar=f"Varianta 20:\n VN= {lb}{', '.join(list(nonterminals[:starting+1]))}{rb},\n VT= {lb}{', '.join(VT)}{rb},\n P={lb}{final}{rb}"
+    print(grammar)
+    grammars.append(grammar)
+
+
+
+for grammar in grammars:
+    rules, VN, VT, F = grammarHelper.grammar_to_language(grammar)
+
+
+
+    lang=language.Language(rules,VN,VT,F)
+
+    lang.generate_word(5,'S')
+    print("Generated words: ",lang.words)
